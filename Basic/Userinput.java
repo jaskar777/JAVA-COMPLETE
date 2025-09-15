e233558494e4cda6059585aa8556a923356c2492
jaskar777 <jaskari@karunya.edu.in>
2025-09-15 16:03:22 +0530

    added

---
+++ Basic/Userinput.java
@@ -0,0 +1,15 @@
+import java.lang.System;
+import java.util.*;
+public class Userinput {
+    public static void main(String[] args){
+
+
+        System.out.println("SYstem is already pre library in java");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int a = sc.nextInt();
+        String b = sc.nextLine();
+
+    }
+}  
---
+++ Basic/Variable.java
@@ -0,0 +1,25 @@
+public class Variable {
+    public static void main(String[] args) {
+        
+        // variable are containers that will help you to store your data. 
+ 
+        int container = 23;
+        System.out.println(container);
+          
+        String container1 = "jaskar";
+        System.out.println(container1);
+
+        char container2 = 'a';
+        System.out.println(container2);
+
+        int a = 100;
+        int b = 200;
+        int c = 300;
+        int s = a + b + c;
+        System.out.println("Total: " + a + b + c);//
+        System.out.println("Total: " + (a + b + c));
+        System.out.println(10 + 12 + 20);
+        System.out.println("Total: "+s);
+
+    }
+}
