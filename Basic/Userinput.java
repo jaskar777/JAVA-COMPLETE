782a8c0ea574a7a588383ea3267a4a3ee14aaf1c
jaskar777 <jaskari@karunya.edu.in>
2025-09-16 19:07:11 +0530

    added

--- Basic/Userinput.java
+++ Basic/Userinput.java
@@ -8,8 +8,13 @@ public class Userinput {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number: ");
+        
         int a = sc.nextInt();
+        //integer after string u must specify scanner nextline 
+        sc.nextLine();
         String b = sc.nextLine();
+        System.out.println("Integer : " + a);
+        System.out.println("Integer after String : " + b);
 
     }
 }  
