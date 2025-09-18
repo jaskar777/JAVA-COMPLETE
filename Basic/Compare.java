5f05693b16c6f323e87e0af60785bf30d32be932
jaskar777 <jaskari@karunya.edu.in>
2025-09-18 11:47:01 +0530

    added

---
+++ Basic/Compare.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Compare {
+    public static void main(String[] args){
+
+        int mark = 20;
+        if((mark > 12) && (mark>11) ){
+            System.out.println("Pass");
+        }
+        else{
+            System.out.println("fail");
+        }
+        
+         
+    }
+}
