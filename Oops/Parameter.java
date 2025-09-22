9f54d9feb16953d9f643462c47edb25aeee8b8a5
jaskar777 <jaskari@karunya.edu.in>
2025-09-22 19:25:56 +0530

    added

--- Oops/Parameter.java
+++ Oops/Parameter.java
@@ -1,12 +1,26 @@
 public class Parameter {
     void soap(int money) {
-        System.out.println("hamam"+ money);
+        System.out.println("hamam" + money);
+        System.out.println("soap purchesed");
+    }
+    
+    void chocolate(String name){
+        System.out.println("Chocolate purchesed");
+        System.out.println(name + " choclate");
+    }
+
+    void sum(int a, int b) {
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Multiplication: " + (a * b));
+        
     }
 
     public static void main(String[] args) {
         
         Parameter obh = new Parameter();
         obh.soap(20);
+        obh.chocolate("diary milk");
+        obh.sum(100, 200);
 
         
     }
---
+++ Oops/Return.java
@@ -0,0 +1,6 @@
+public class Return {
+    public static void main(String[] args) {
+        
+        
+    }
+}
