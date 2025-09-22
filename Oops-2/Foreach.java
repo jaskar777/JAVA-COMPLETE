7fc5cec713cc42d1cc9c1b62e6f5777e0c72509c
jaskar777 <jaskari@karunya.edu.in>
2025-09-22 23:16:18 +0530

    added

--- Oops-2/Foreach.java
+++ Oops-2/Foreach.java
@@ -1,6 +1,24 @@
 public class Foreach {
     public static void main(String[] args) {
         
+        int[] marks = { 1, 10, 203, 23, 2, 34, 200 };
+        
+
+        //array start with zero and less than only give <= --> out of bond exceptation 
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+        
+        for (int a : marks) {
+            System.out.println(a);
+        }
+
+        String[] b = { "apple", "mango", "banana" };
+        for (String c : b) {
+            System.out.println(c);
+        }
+
+
 
     }
 }
