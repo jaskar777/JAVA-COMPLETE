2d9e002f2eb5cf7a2f42de208cbe4b9cbc2a0e83
jaskar777 <jaskari@karunya.edu.in>
2025-09-22 09:56:54 +0530

    added

--- Basic/Pattern.java
+++ Basic/Pattern.java
@@ -1,6 +1,21 @@
 public class Pattern {
     public static void main(String[] args) {
         
-        
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= i; j++) {// in i =1,2,3
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("for loop -----");
+        for (int a = 1; a <= 3; a++) {
+            for (int b = 1; b <= 3; b++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
     }
 }
