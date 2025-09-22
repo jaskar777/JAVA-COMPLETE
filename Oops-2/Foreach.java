430b23371a1e0e93155a86e1d0ef04d35400714f
jaskar777 <jaskari@karunya.edu.in>
2025-09-22 22:49:31 +0530

    added

---
+++ Oops-2/Foreach.java
@@ -0,0 +1,6 @@
+public class Foreach {
+    public static void main(String[] args) {
+        
+
+    }
+}
--- Oops-2/Method_overloading.java
+++ Oops-2/Method_overloading.java
@@ -2,10 +2,23 @@ public class Method_overloading {
 
 
     void display() {
-        
+        System.out.println("Hello world ");
+    }
+
+    //function name is ok but paramter is must be different
+    void display(int a){
+        System.out.println("hell-2");
+    }
+
+    void display(int i, int j) {
+        System.out.println("Hello-3-- total: "+(i+j));
     }
     public static void main(String[] args){
 
+        Method_overloading obj = new Method_overloading();
+        obj.display();
+        obj.display(2);
+        obj.display(100,200);
 
     }
 }
