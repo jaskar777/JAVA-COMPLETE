f2e4705eafcade5b82dc43a0f2fade900df2bec1
jaskar777 <jaskari@karunya.edu.in>
2025-09-24 20:27:21 +0530

    added

--- Oops-2/Parametercons.java
+++ Oops-2/Parametercons.java
@@ -1,6 +1,29 @@
+
+
 public class Parametercons {
+
+    int marks;
+    String name;
+
+    Parametercons(int a) {
+
+        System.out.println("Hello");
+        System.out.println(a);
+    }
+    
+    Parametercons() {
+
+        System.out.println("Hello-2");
+        
+    }
+
+    
     public static void main(String[] args){
 
+        Parametercons obj = new Parametercons(100);
+
+        Parametercons obj1 = new Parametercons();
+        
         
     }
 }
