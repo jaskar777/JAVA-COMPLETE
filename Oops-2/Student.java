deb283a3803f82e0150b15b1e499e6499de106f7
jaskar777 <jaskari@karunya.edu.in>
2025-09-24 20:44:10 +0530

    added

--- Oops-2/Student.java
+++ Oops-2/Student.java
@@ -1,7 +1,21 @@
 public class Student {
+
+    int mark;
+    String name;
+
+    Student(int a, String b) {
+        mark = a;
+        name = b;
+
+    }
+
+    
     public static void main(String[] args) {
         
+        Student obj = new Student(100, "jaskar");
+        System.out.println(obj.mark);
+        System.out.println(obj.name);
 
-        
+
     }
 }
