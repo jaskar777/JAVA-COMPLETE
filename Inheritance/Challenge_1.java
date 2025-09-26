e03be0c07b02de00dccaf9eb2ee45e14a0b947a9
jaskar777 <jaskari@karunya.edu.in>
2025-09-26 23:34:50 +0530

    added

--- Inheritance/Challenge_1.java
+++ Inheritance/Challenge_1.java
@@ -1,6 +1,21 @@
-public class Challenge_1 {
-    public static void main(String[]  args){
+class Animal {
+    String name;
+    int age;
 
+    void makesound() {
+        System.out.println("Animal make sound");
+    }
+}
+
+
+public class Challenge_1 {
+    public static void main(String[] args) {
         
+        Animal obj = new Animal();
+        obj.name = "lion";
+        obj.age = 10;
+        obj.makesound();
+        System.out.println(obj.name);
+
     }
 }
