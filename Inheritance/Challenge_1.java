40ac24219f507ce4190e396e3dab8d988e0306f8
jaskar777 <jaskari@karunya.edu.in>
2025-09-27 14:11:30 +0530

    added

--- Inheritance/Challenge_1.java
+++ Inheritance/Challenge_1.java
@@ -8,14 +8,58 @@ class Animal {
 }
 
 
+class Dog extends Animal {
+
+    String breed;
+
+    @Override //optional if not use it will run
+    void makesound() {
+        System.out.println("Dog barks");
+    }
+
+    void fetch() {
+        System.out.println("Dog is fetching");
+    }
+
+}
+
+class Cat extends Animal {
+    String color;
+
+    @Override
+    void makesound() {
+        System.out.println("cat meow");
+    }
+
+    void climb() {
+        System.out.println("cat climb");
+    }
+}
 public class Challenge_1 {
     public static void main(String[] args) {
         
-        Animal obj = new Animal();
-        obj.name = "lion";
-        obj.age = 10;
-        obj.makesound();
-        System.out.println(obj.name);
+        // Animal obj = new Animal();
+        // obj.name = "lion";
+        // obj.age = 10;
+        // obj.makesound();
+        // System.out.println(obj.name);
+
+        Dog d1 = new Dog();
+        d1.name = "tiger";
+        d1.age = 2;
+        d1.breed = "labrador";
+        System.out.println(d1.name);
+        d1.makesound();
+        d1.fetch();
+
+        //another object 
+
+        Cat c1 = new Cat();
+        c1.age = 10;
+        c1.name = "orange cat";
+        c1.color = "orange";
+        c1.makesound();
+        c1.climb();
 
     }
 }
---
+++ Oops-3/Super.java
@@ -0,0 +1,10 @@
+
+//super keyword is child class is access to parent class
+public class Super {
+    
+    public static void main(String[] args) {
+        
+
+        
+    }
+}
