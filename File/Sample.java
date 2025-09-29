632d30f500bf677f9249f6913e4aec25196f4a5e
jaskar777 <jaskari@karunya.edu.in>
2025-09-29 18:25:12 +0530

    added

---
+++ File/Sample.java
@@ -0,0 +1,8 @@
+import java.io.FileWriter;
+
+public class Sample {
+    public static void main(String[] args) {
+        
+
+    }
+}
--- Oops-4/Throws.java
+++ Oops-4/Throws.java
@@ -1,6 +1,17 @@
+class Calculator {
+    void div() throws Exception {
+        int a = 10 / 0;
+    }
+}
+
 public class Throws {
-    public static void main(String[] args) {
-        
-        
+    public static void main(String[]  args) {
+
+        Calculator obj = new Calculator();
+        try {
+            obj.div();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
